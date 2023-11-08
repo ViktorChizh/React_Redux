b8b3a64a2c36628f8ac1858115d23d8cc1322a39
ViktorChizh <victor.chizh@mail.ru>
2023-11-08 13:50:57 +0300

    update /docs

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,6 @@ import s from './Header.module.css';
 const Header = () => {
     // eslint-disable-next-line react/jsx-no-comment-textnodes
     return <header className={s.header}>
-        // eslint-disable-next-line jsx-a11y/alt-text
         <img style={{ width: 60 + 'px' }} src='./../../favicon.ico' />
     </header>
 }
